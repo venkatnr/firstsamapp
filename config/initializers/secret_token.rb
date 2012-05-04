a2ab7c896d9d17b1fba7790b441524c63465b432
venkat <nishankararamu@gmail.com>
2012-05-04 08:26:37 +0530

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstsamapp::Application.config.secret_token = '2a4ca2cde9fbe1e05cad83807557dbacfa1e2532d69e918250f7d4773ef89e69748d42f82f9e873c1c80597ac499ca42256b7717e4697954283afbb878c57495'
